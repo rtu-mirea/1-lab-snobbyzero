4465ab1665b2575ea4a26d10eecf8bf9bf20f168
Ildar Magzhanov <ildar200103@rambler.ru>
2019-09-07 11:58:25 +0300

    toString, .gitignore

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.age = 42;
+        student.name = "Tim";
+        student.isAlive = false;
+
+        System.out.println(student);
+    }
+}
+
+class Student {
+    public String name;
+    public int age;
+    public boolean isAlive;
+}
